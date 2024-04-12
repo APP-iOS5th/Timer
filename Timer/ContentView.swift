9946423ffaf0e3ca5bfaf753d7381bce62d48f14
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-12 14:28:21 +0900

    time reduce question

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -58,43 +58,82 @@ struct ContentView: View {
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
                 
                 Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (10 * 60))
                     .stroke(Color.blue, lineWidth: 10)
                     .rotationEffect(.degrees(-90))
                 
                 VStack {
-                    Button {
-                        switch timeRemaining {
-                        case 0..<180:
-                            timeRemaining = 180
-                        case 180..<300:
-                            timeRemaining = 300
-                        case 300..<420:
-                            timeRemaining = 420
-                        case 300..<600:
-                            timeRemaining = 600
-                        case 600..<900:
-                            timeRemaining = 900
-                        case 900..<1200:
-                            timeRemaining = 1200
-                        case 1200..<1500:
-                            timeRemaining = 1500
-                        case 1500..<1800:
-                            timeRemaining = 1800
-                        default:
-                            timeRemaining = 0
-                        }
-
-                    } label: {
                         Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
                             .font(.system(size: 20, weight: .bold))
-                    }
-                    .buttonStyle(PlainButtonStyle())
+                            .offset(x:0, y: 25)
                     Button {
                         isRunning.toggle()
                     } label: {
                         Image(systemName: isRunning ? "pause" : "play.fill")
                     }
+                    .offset(x: 0, y:25)
+                    
+                    Button {
+                        switch timeRemaining {
+                        case 0..<60:
+                            timeRemaining = 60
+                        case 60..<120:
+                            timeRemaining = 120
+                        case 120..<180:
+                            timeRemaining = 180
+                        case 180..<240:
+                            timeRemaining = 240
+                        case 240..<300:
+                            timeRemaining = 300
+                        case 300..<360:
+                            timeRemaining = 360
+                        case 360..<420:
+                            timeRemaining = 420
+                        case 420..<480:
+                            timeRemaining = 480
+                        case 480..<540:
+                            timeRemaining = 540
+                        case 540..<600:
+                            timeRemaining = 600
+                        default:
+                            timeRemaining = 0
+                        }
+                    } label: {
+                        Image(systemName: "plus.square.fill")
+                    }
+                    .buttonStyle(.borderless)
+                    .offset(x: 50, y: -70)
+                    
+                    Button {
+                        switch timeRemaining {
+                        case 540..<600:
+                            timeRemaining = 540
+                        case 480..<540:
+                            timeRemaining = 480
+                        case 420..<480:
+                            timeRemaining = 420
+                        case 360..<420:
+                            timeRemaining = 360
+                        case 300..<360:
+                            timeRemaining = 300
+                        case 240..<300:
+                            timeRemaining = 240
+                        case 180..<240:
+                            timeRemaining = 180
+                        case 120..<180:
+                            timeRemaining = 120
+                        case 60..<120:
+                            timeRemaining = 60
+                        case 0..<60:
+                            timeRemaining = 0
+                        default:
+                            timeRemaining = 0
+                        }
+                    } label: {
+                        Image(systemName: "minus.square.fill")
+                    }
+                    .buttonStyle(.borderless)
+                    .offset(x: 50, y: 6)
                 }
             }
                 
