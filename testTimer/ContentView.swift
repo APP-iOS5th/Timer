797d5a78c6effcb7ff8ac70a5578250a6a648f15
wnylxx <hyy96hyy@naver.com>
2024-04-12 13:00:58 +0900

    Initial Commit

---
+++ testTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testTimer
+//
+//  Created by wonyoul heo on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testTimer/testTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTimerApp.swift
+//  testTimer
+//
+//  Created by wonyoul heo on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct testTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
