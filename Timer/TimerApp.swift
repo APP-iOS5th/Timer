6161aa737832c928d3f9fff49792742408fb2d8d
Shiene Park <logintolookupsome@gmail.com>
2024-04-12 10:02:14 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by mini on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
