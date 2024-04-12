4371d91a69734c2d054290b8a1603b10fd0c10dd
jinwoong <baseball0020@gmail.com>
2024-04-12 13:01:59 +0900

    feat: add Timer view skelton

--- Timer/TimerView.swift
+++ Timer/TimerView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct TimerView: View {
-    
     var body: some View {
-        EmptyView()
+        ZStack {
+            Circle()
+                .stroke(
+                    style: .init(
+                        lineWidth: 8,
+                        dash: [1, 3]
+                    )
+                )
+                .rotation(.degrees(-90))
+                .foregroundStyle(.accent)
+                .padding()
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "x.circle.fill")
+                        .foregroundStyle(.gray)
+                        .font(.system(size: 28))
+                }
+                .buttonStyle(.plain)
+                
+                Spacer()
+                
+                Button {
+                
+                } label: {
+                    Image(systemName: "play.circle.fill")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.black, .accent)
+                        .font(.system(size: 28))
+                }
+                .buttonStyle(.plain)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .frame(width: 200, height: 200)
+        .background(Color.black)
     }
+}
+
+#Preview {
+    TimerView()
 }
