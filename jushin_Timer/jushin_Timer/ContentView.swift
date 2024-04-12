e3679099602fae28881b12a4bd8ae5131b02c28b
shin-jae-uk <shinju1412@gmail.com>
2024-04-12 14:33:30 +0900

    Clone coding (Origin ver.)

--- jushin_Timer/jushin_Timer/ContentView.swift
+++ jushin_Timer/jushin_Timer/ContentView.swift
@@ -6,13 +6,109 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+struct AlwaysOnTopView: NSViewRepresentable {
+    let window: NSWindow
+    let isAlwaysOnTop: Bool
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {
+        if isAlwaysOnTop {
+            window.level = .floating
+        } else {
+            window.level = .normal
+        }
+    }
+}
+
+class SoundManager {
+    static let instance = SoundManager()
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "Beep", withExtension: "mov") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("재생에 오류가 발생했습니다. \(error.localizedDescription)")
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var isRunning = false
+    @State private var timeRemaining = 0
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
-            
+            ZStack {
+                Circle()
+                    .stroke(Color.white.opacity(0.5), lineWidth: 20)
+                
+                Circle()
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .stroke(Color.red, lineWidth: 20)
+                    .rotationEffect(.degrees(-90))
+                
+                VStack {
+                    Button {
+                        switch timeRemaining {
+                        case 0..<180:
+                            timeRemaining = 180
+                        case 180..<300:
+                            timeRemaining = 300
+                        case 300..<420:
+                            timeRemaining = 420
+                        case 300..<600:
+                            timeRemaining = 600
+                        case 600..<900:
+                            timeRemaining = 900
+                        case 900..<1200:
+                            timeRemaining = 1200
+                        case 1200..<1500:
+                            timeRemaining = 1500
+                        case 1500..<1800:
+                            timeRemaining = 1800
+                        default:
+                            timeRemaining = 0
+                        }
+
+                    } label: {
+                        Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
+                            .font(.system(size: 40, weight: .bold))
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Button {
+                        isRunning.toggle()
+                    } label: {
+                        Image(systemName: isRunning ? "pause" : "play.fill")
+                    }
+                }
+                
+            }
         }
+        .frame(width:200, height: 200)
         .padding()
+        .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
+        .onReceive(timer) { _ in
+            if isRunning && timeRemaining > 0 {
+                timeRemaining -= 1
+                if timeRemaining <= 10 {
+                    NSSound.beep()
+                }
+            } else if isRunning {
+                isRunning = false
+            }
+        }
     }
 }
 
