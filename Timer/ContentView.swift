57f84073bd7fe5ed8f7ec835d3a18e5a7897917d
Jongchan Yun <jcyun1996@gmail.com>
2024-04-12 15:47:02 +0900

    최대 10분, 클릭 시 1분 추가됨

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -58,29 +58,33 @@ struct ContentView: View {
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
                 
                 Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (10 * 60))
                     .stroke(Color.blue, lineWidth: 10)
                     .rotationEffect(.degrees(-90))
                 
                 VStack {
                     Button {
                         switch timeRemaining {
-                        case 0..<180:
+                        case 0..<60:
+                            timeRemaining = 60
+                        case 60..<120:
+                            timeRemaining = 120
+                        case 120..<180:
                             timeRemaining = 180
-                        case 180..<300:
+                        case 180..<240:
+                            timeRemaining = 240
+                        case 240..<300:
                             timeRemaining = 300
-                        case 300..<420:
+                        case 300..<360:
+                            timeRemaining = 360
+                        case 360..<420:
                             timeRemaining = 420
-                        case 300..<600:
+                        case 420..<480:
+                            timeRemaining = 480
+                        case 480..<540:
+                            timeRemaining = 540
+                        case 540..<600:
                             timeRemaining = 600
-                        case 600..<900:
-                            timeRemaining = 900
-                        case 900..<1200:
-                            timeRemaining = 1200
-                        case 1200..<1500:
-                            timeRemaining = 1500
-                        case 1500..<1800:
-                            timeRemaining = 1800
                         default:
                             timeRemaining = 0
                         }
