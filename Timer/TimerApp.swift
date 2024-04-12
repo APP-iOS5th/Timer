d2b846d86759a5044580d26e9f9af360578d3835
Hyunseo Yook <hhh3332@naver.com>
2024-04-12 11:57:37 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 육현서 on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
