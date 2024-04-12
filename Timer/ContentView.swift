9257b631e65536ccb01b47cf7f7f67f1274fa405
DopeKS <kyusang.dev@gmail.com>
2024-04-12 11:14:18 +0900

    set variables

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,18 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRunning = false
+    @State private var timeRemaining = 100
+    
     var body: some View {
         VStack {
             ZStack {
                 Circle()
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                Circle()
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .stroke(Color.blue, lineWidth: 10)
+                    .rotationEffect(.degrees(-90))
                 VStack {
-                    Text("00:00")
+                    Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
                         .font(.system(size: 20, weight: .bold))
                     Button {
-                        
+                        isRunning.toggle()
                     } label: {
-                        Image(systemName: "play.fill")
+                        Image(systemName: isRunning ? "pause" : "play.fill")
                     }
                 }
             }
