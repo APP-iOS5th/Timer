24a65e5fb77e0ee10697e7e067ce74a4b860ed11
Shiene Park <logintolookupsome@gmail.com>
2024-04-12 17:00:46 +0900

    add timer

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -11,11 +11,17 @@ struct ContentView: View {
     @State private var isRunning = false
     @State private var timeRemaining = 100
     
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
             ZStack {
                 Circle()
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                Circle()
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .stroke(Color.blue, lineWidth: 10)
+                    .rotationEffect(.degrees(-90))
                 VStack {
                     Text("\(timeRemaining / 60): \(String(format: "%02d", timeRemaining % 60))")
                         .font(.system(size: 20, weight: .bold))
@@ -30,6 +36,13 @@ struct ContentView: View {
         }
         .frame(width: 100, height: 100)
         .padding()
+        .onReceive(timer) { _ in
+            if isRunning && timeRemaining > 0 {
+                timeRemaining -= 1
+            } else if isRunning {
+                isRunning = false
+            }
+        }
         
     }
 }
