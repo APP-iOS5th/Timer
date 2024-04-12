fd5af38e5cee2353cf4ffd411d93ed4c2c81ab13
avan31017 <avan31017@gmail.com>
2024-04-12 17:22:19 +0900

    Timer SBJ

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -2,12 +2,125 @@
 //  ContentView.swift
 //  Timer
 //
-//  Created by Jungman Bae on 4/12/24.
+//  Created by 조성빈 on 4/12/24.
 //
 
 import SwiftUI
-import AVFoundation
 
+struct ContentView: View {
+    
+    @State private var isRunning = false
+    @State private var timeRemaining = 0
+    @State private var defaultTime = 0
+    
+    @State private var alarmAry: [alarmModel] = []
+    
+    // every 마다 이벤트를 발생시킴
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 20)
+                Circle()
+                    .trim(from: 0, to: CGFloat(timeRemaining) / CGFloat(defaultTime))
+                    .stroke(
+                        CGFloat(timeRemaining) / CGFloat(defaultTime) > 0.66 ? .green : CGFloat(timeRemaining) / CGFloat(defaultTime) > 0.33 ? .orange : .red
+                        , lineWidth: 10)
+                    .rotationEffect(.degrees(-90))
+                    .animation(Animation.linear(duration: 1), value: timeRemaining)
+                HStack {
+                    Button(action: {
+                        timeRemaining += 10
+                        defaultTime += 10
+                    }) {
+                        Image(systemName: "10.circle")
+                            .font(.system(size: 25))
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                    Button(action: {
+                        alarmAry.append(alarmModel(id: UUID(), min: timeRemaining / 60, sec: timeRemaining % 60))
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 25))
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+                .frame(width: 220)
+                .position(x: 100, y: 5)
+                VStack(spacing: 10) {
+                    Text("\(String(format: "%02d", timeRemaining / 60)) : \(String(format: "%02d", timeRemaining % 60))")
+                        .font(.system(size: 35, weight: .bold))
+                    HStack(spacing: 13) {
+                        Button(action: {
+                            timeRemaining += 60
+                            defaultTime += 60
+                        }) {
+                            Text("1분")
+                        }
+                        Button(action: {
+                            timeRemaining += 300
+                            defaultTime += 300
+                        }) {
+                            Text("5분")
+                        }
+                        Button(action: {
+                            timeRemaining += 600
+                            defaultTime += 600
+                        }) {
+                            Text("10분")
+                        }
+                    }
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            isRunning.toggle()
+                        }) {
+                            Image(systemName: isRunning ? "pause.fill" : "play.fill")
+                                .font(.system(size: 30))
+                        }
+                        .disabled(timeRemaining > 0 ? false : true)
+                        .buttonStyle(PlainButtonStyle())
+                        Button(action: {
+                            isRunning = false
+                            timeRemaining = 0
+                            defaultTime = 0
+                        }) {
+                            Image(systemName: "square.fill")
+                                .font(.system(size: 25))
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    .padding(.top, 10)
+                }
+            }
+            .frame(width: 200, height: 200)
+            .padding()
+            .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
+            .onReceive(timer) { _ in
+                if isRunning && timeRemaining > 0 {
+                    timeRemaining -= 1
+                }else if isRunning {
+                    isRunning = false
+                    defaultTime = 0
+                    NSSound.beep()
+                }
+            }
+            Rectangle()
+                .frame(width: 200, height: 1)
+                .opacity(0.2)
+            ForEach(alarmAry, id: \.id) { data in
+                alarmList(alarmAry: $alarmAry, min: data.min, sec: data.sec, index: alarmAry.firstIndex(of: data) ?? 0)
+                    .onTapGesture {
+                        timeRemaining = (data.min * 60) + data.sec
+                        defaultTime = timeRemaining
+                        isRunning = false
+                    }
+            }
+        }
+    }
+}
 
 struct AlwaysOnTopView: NSViewRepresentable {
     let window: NSWindow
@@ -27,93 +140,6 @@ struct AlwaysOnTopView: NSViewRepresentable {
     }
 }
 
-
-class SoundManager {
-    static let instance = SoundManager()
-    var player: AVAudioPlayer?
-    
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "Beep", withExtension: "mov") else { return }
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            player?.play()
-        } catch let error {
-            print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
-        }
-    }
-}
-
-
-struct ContentView: View {
-    @State private var isRunning = false
-    @State private var timeRemaining = 0
-    
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    
-    var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
-                
-                Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
-                    .stroke(Color.blue, lineWidth: 10)
-                    .rotationEffect(.degrees(-90))
-                
-                VStack {
-                    Button {
-                        switch timeRemaining {
-                        case 0..<180:
-                            timeRemaining = 180
-                        case 180..<300:
-                            timeRemaining = 300
-                        case 300..<420:
-                            timeRemaining = 420
-                        case 300..<600:
-                            timeRemaining = 600
-                        case 600..<900:
-                            timeRemaining = 900
-                        case 900..<1200:
-                            timeRemaining = 1200
-                        case 1200..<1500:
-                            timeRemaining = 1500
-                        case 1500..<1800:
-                            timeRemaining = 1800
-                        default:
-                            timeRemaining = 0
-                        }
-
-                    } label: {
-                        Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
-                            .font(.system(size: 20, weight: .bold))
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    Button {
-                        isRunning.toggle()
-                    } label: {
-                        Image(systemName: isRunning ? "pause" : "play.fill")
-                    }
-                }
-            }
-                
-        }
-        .frame(width: 100, height: 100)
-        .padding()
-        .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
-        .onReceive(timer) { _ in
-            if isRunning && timeRemaining > 0 {
-                timeRemaining -= 1
-                if timeRemaining <= 10 {
-                    NSSound.beep()
-                }
-            } else if isRunning {
-                isRunning = false
-            }
-        }
-    }
-}
 
 #Preview {
     ContentView()
