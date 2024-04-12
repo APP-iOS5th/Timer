f3489d9262919ee814d91cac17f7477207af8d2e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-12 12:30:16 +0900

    Sound

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+class SoundManger {
+    static let instance = SoundManger()
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        guard let url  = Bundle.main.url(forResource: "Coin 1" , withExtension: "mov")
+        else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")//localizedDescription: 오류에 대한 발생 이유를 설명함
+        }
+    }
+}
 
 struct ContentView: View {
     @State private var isRunning = false
-    @State private var timeRemaining = 1 //타이머의 남은 시간을 추적
+    @State private var timeRemaining = 10 //타이머의 남은 시간을 추적
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
