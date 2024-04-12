aaadbe359a412cc3f87f1e360d2ded519b877a61
jinwoong <baseball0020@gmail.com>
2024-04-12 23:09:59 +0900

    feat: implement text field for numeric value

--- Timer/View/Component/NumericTextField.swift
+++ Timer/View/Component/NumericTextField.swift
@@ -6,13 +6,32 @@
 //
 
 import SwiftUI
+import Combine
 
 struct NumericTextField: View {
+    @State private var text: String = ""
+    @Binding var number: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("", text: $text)
+            .font(.system(size: 36))
+            .textFieldStyle(NumericTextFieldStyle())
+            .onReceive(Just(text)) { value in
+                let filtered = value.filter(CharacterSet.decimalDigits.contains(_:))
+                if filtered != value {
+                    self.text = filtered
+                }
+                if let number = Int(filtered) {
+                    self.number = number
+                } else {
+                    self.number = 0
+                }
+            }
     }
 }
 
 #Preview {
-    NumericTextField()
+    NumericTextField(
+        number: .constant(0)
+    )
 }
