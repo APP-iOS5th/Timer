32e0e51c6e32b6560c3bed690127f3f74d67ea1e
jinwoong <baseball0020@gmail.com>
2024-04-12 11:16:17 +0900

    chore: add TimerView

---
+++ Timer/TimerView.swift
@@ -0,0 +1,15 @@
+//
+//  TimerView.swift
+//  Timer
+//
+//  Created by jinwoong Kim on 4/12/24.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    
+    var body: some View {
+        EmptyView()
+    }
+}
