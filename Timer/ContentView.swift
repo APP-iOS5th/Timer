0008e1fb5d2b84da69ff43de9f207af9ba6a589b
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-12 11:25:12 +0900

    set variables

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,25 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRunning = false
+    @State private var timeRemaining = 100 //타이머의 남은 시간을 추적
     var body: some View {
         VStack {
             ZStack{
                 Circle()
+                    .trim(from: 0, to: CGFloat(timeRemaining)/(30 * 60)) //특정 부분만 남기고 나머지 부분 절단
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                    .rotationEffect(.degrees(-90))
                 VStack {
-                    Text("00:00")
-                        .font(.system(size:20, weight: .bold))
+                    Text("\(timeRemaining/60) : \(String(format: "%02d", timeRemaining % 60))")
+                        .font(.system(size:20, weight: .regular))
                     Button {
-                        
+                        isRunning.toggle()
                     } label: {
-                        Image(systemName: "play.fill")
+                        Image(systemName: isRunning ? "pause" : "play.fill")
                     }
                 }
                 
             }
 
         }
-        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+        .frame(width: 100, height: 100)
         .padding()
     }
 }
