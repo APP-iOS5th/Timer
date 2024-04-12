d0902c9bc72e105f29993df2d73d19885007f544
YuleGlycerine <figurefig13@gmail.com>
2024-04-12 11:15:10 +0900

    Timer tutorial done

--- TIMER_T/ContentView.swift
+++ TIMER_T/ContentView.swift
@@ -4,16 +4,47 @@
 //
 //  Created by Mac on 4/12/24.
 //
-
 import SwiftUI
 import AVFoundation
 
 
+struct AlwaysOnTopView: NSViewRepresentable {
+    let window: NSWindow
+    let isAlwaysOnTop: Bool
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {
+        if isAlwaysOnTop {
+            window.level = .floating
+        } else {
+            window.level = .normal
+        }
+    }
+}
 
 
+class SoundManager {
+    static let instance = SoundManager()
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "Beep", withExtension: "mov") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
+        }
+    }
+}
 struct ContentView: View {
     @State private var isRunning = false
-    @State private var timeRemaining = 10
+    @State private var timeRemaining = 0
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -21,48 +52,64 @@ struct ContentView: View {
         VStack {
             ZStack {
                 Circle()
-                    .stroke(Color.gray.opacity(0.2),lineWidth: 10)
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
                 
                 Circle()
                     .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
                     .stroke(Color.blue, lineWidth: 10)
                     .rotationEffect(.degrees(-90))
-               
+                
                 VStack {
-                    Buttton {
-                        
+                    Button {
+                        switch timeRemaining {
+                        case 0..<180:
+                            timeRemaining = 180
+                        case 180..<300:
+                            timeRemaining = 300
+                        case 300..<420:
+                            timeRemaining = 420
+                        case 300..<600:
+                            timeRemaining = 600
+                        case 600..<900:
+                            timeRemaining = 900
+                        case 900..<1200:
+                            timeRemaining = 1200
+                        case 1200..<1500:
+                            timeRemaining = 1500
+                        case 1500..<1800:
+                            timeRemaining = 1800
+                        default:
+                            timeRemaining = 0
+                        }
                     } label: {
                         Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
                             .font(.system(size: 20, weight: .bold))
                     }
+                    .buttonStyle(PlainButtonStyle())
                     Button {
                         isRunning.toggle()
                     } label: {
                         Image(systemName: isRunning ? "pause" : "play.fill")
-                          //  .buttonStyle(PlainButtonStyle())
                     }
                 }
-                
             }
             
         }
         .frame(width: 100, height: 100)
         .padding()
+        .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
         .onReceive(timer) { _ in
             if isRunning && timeRemaining > 0 {
                 timeRemaining -= 1
                 if timeRemaining <= 10 {
                     NSSound.beep()
                 }
-                
             } else if isRunning {
                 isRunning = false
-                NSSound.beep()
             }
         }
     }
 }
-
 #Preview {
     ContentView()
 }
