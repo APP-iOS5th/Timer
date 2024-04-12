97bee02073025fd81123754ad7537267a7585fd6
jinwoong <baseball0020@gmail.com>
2024-04-12 16:44:21 +0900

    feat: add Timer cell for list view

--- Timer/View/Component/TimerCell.swift
+++ Timer/View/Component/TimerCell.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct TimerCell: View {
+    private let totalTime: Int
+    private let remainingTime: Int
+    
+    init(totalTime: Int, remainingTime: Int) {
+        self.totalTime = totalTime
+        self.remainingTime = remainingTime
+    }
+    
+    init(_ time: Int) {
+        self.totalTime = time
+        self.remainingTime = time
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            TimerTickView(totalTime: totalTime, remainingTime: remainingTime)
+            Circle()
+                .rotation(.degrees(-90))
+                .foregroundStyle(Color("DarkGray"))
+                .padding(20)
+            InitialTimeLabel(time: totalTime)
+        }
+        .frame(width: 150, height: 150)
+        .background(Color.black)
     }
 }
 
 #Preview {
-    TimerCell()
+    TimerCell(
+        totalTime: 30,
+        remainingTime: 30
+    )
 }
