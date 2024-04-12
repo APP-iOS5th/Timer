6027dccbb382104296fa722f9224c36cd7082a89
Jinrev <geneikj@gmail.com>
2024-04-12 10:02:42 +0900

    Initial Commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by 김국진 on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
