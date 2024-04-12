dc875d8351fea195114c1eb7e2fb3574253a5195
jinwoong <baseball0020@gmail.com>
2024-04-12 16:51:30 +0900

    feat: add `TimerListViewModel`

---
+++ Timer/ViewModel/TimerListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  TimerListViewModel.swift
+//  Timer
+//
+//  Created by jinwoong Kim on 4/12/24.
+//
+
+import Foundation
+
+@Observable
+final class TimerListViewModel {
+    // MARK: States
+    private(set) var timerInfo: [TimerInfo] = [
+        .init(time: 30),
+        .init(time: 180),
+        .init(time: 10),
+        .init(time: 50),
+        .init(time: 120),
+    ]
+    
+    // MARK: Actions and Reduces
+    // TODO: Add methods for appending, removing timer information.
+    
+    
+}
