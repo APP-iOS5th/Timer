f3c420ecd50ee56edb6539ea56979121d2a0a708
Shiene Park <logintolookupsome@gmail.com>
2024-04-12 17:02:38 +0900

    change value of timeRemaining

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isRunning = false
-    @State private var timeRemaining = 100
+    @State private var timeRemaining = 1
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -19,7 +19,7 @@ struct ContentView: View {
                 Circle()
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
                 Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (5 * 60))
                     .stroke(Color.blue, lineWidth: 10)
                     .rotationEffect(.degrees(-90))
                 VStack {
