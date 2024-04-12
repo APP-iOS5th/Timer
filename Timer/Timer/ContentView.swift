86b7260c6595f36834a7b23852f3e8596e0b3d1e
uunwon <chea2089@naver.com>
2024-04-12 11:10:27 +0900

    base frame

--- Timer/Timer/ContentView.swift
+++ Timer/Timer/ContentView.swift
@@ -7,14 +7,26 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .stroke(Color.customPink.opacity(0.9), lineWidth: 10)
+                VStack {
+                    Text("00:00")
+                        .font(.system(size: 20, weight: .bold))
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .imageScale(.large)
+                    }
+                }
+            }
         }
+        .frame(width: 100, height: 100)
         .padding()
     }
 }
