55dcc9bec509985ddc707272dece9133f3bf0cad
Jin Park <jinpark1504@gmail.com>
2024-04-12 14:44:00 +0900

    Narration

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -6,14 +6,91 @@
 //
 
 import SwiftUI
+import AVFoundation
 
+struct AlwaysOnTopView: NSViewRepresentable {
+    let window: NSWindow
+    let isAlwaysOnTop: Bool
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        return view
+    }
+    func updateNSView(_ nsView: NSView, context: Context) {
+        if isAlwaysOnTop {
+            window.level = .floating
+        } else {
+            window.level = .normal
+        }
+    }
+}
 struct ContentView: View {
+    @State private var initialMinutes = 1
+    @State private var remainingSeconds = 5
+    @State private var isRunning = false
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let speechSynthesizer = AVSpeechSynthesizer()
+    
+    func speech(_ speak: String) {
+        let utterance = AVSpeechUtterance(string: speak)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        self.speechSynthesizer.speak(utterance)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                Circle()
+                    .trim(from: 0, to: CGFloat(remainingSeconds) / (CGFloat(initialMinutes) * 60))
+                    .stroke(Color.red, lineWidth: 10)
+                    .rotationEffect(.degrees(-90))
+                    
+                VStack {
+                    Text("\(remainingSeconds / 60) : \(String(format: "%02d", remainingSeconds % 60))")
+                        .font(.system(size: 20, weight: .bold))
+                    Button {
+                        isRunning.toggle()
+                    } label: {
+                        Image(systemName: isRunning ? "pause" : "play.fill")
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .font(.system(size: 20, weight: .bold))
+                    Picker("", selection: $initialMinutes, content: {
+                        if isRunning {
+                            Text("Running")
+                        } else {
+                            ForEach([1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55,60], id:\.self) { index in
+                                Text("\(index)").tag("\(index)")
+                            }
+                        }
+                    })
+                    .frame(width: 60)
+                    .pickerStyle(.menu)
+                    .onChange(of: initialMinutes) { oldValue, newValue in
+                        remainingSeconds = initialMinutes * 60
+                    }
+                    .padding(.trailing, 6)
+                }
+            }
+        }
+        .frame(width: 100, height: 100)
+        .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
+        .onReceive(timer) { _ in
+            if isRunning && remainingSeconds > 0 {
+                remainingSeconds -= 1
+                if remainingSeconds < 4 {
+                    print("\(remainingSeconds)")
+                    self.speech("\(remainingSeconds)")
+                }
+            }
+            if remainingSeconds == 0 && isRunning {
+                self.speech("그만")
+                remainingSeconds = initialMinutes * 60
+                isRunning = false
+            }
         }
         .padding()
     }
