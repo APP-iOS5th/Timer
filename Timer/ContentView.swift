a5cc549b2c262fe79fc59e8c8d23ea57070c8e62
DopeKS <kyusang.dev@gmail.com>
2024-04-12 11:16:44 +0900

    add timer

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isRunning = false
-    @State private var timeRemaining = 100
+    @State private var timeRemaining = 1
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack {
@@ -17,7 +19,7 @@ struct ContentView: View {
                 Circle()
                     .stroke(Color.gray.opacity(0.2), lineWidth: 10)
                 Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                    .trim(from: 0, to: CGFloat(timeRemaining) / (5 * 60))
                     .stroke(Color.blue, lineWidth: 10)
                     .rotationEffect(.degrees(-90))
                 VStack {
@@ -33,6 +35,13 @@ struct ContentView: View {
         }
         .frame(width: 100, height: 100)
         .padding()
+        .onReceive(timer) { _ in
+            if isRunning && timeRemaining > 0 {
+                timeRemaining -= 1
+            } else if isRunning {
+                isRunning = false
+            }
+        }
     }
 }
 
