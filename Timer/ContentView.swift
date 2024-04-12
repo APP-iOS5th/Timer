e09666bb58eb2383432a09afd85241ae5156105a
DopeKS <kyusang.dev@gmail.com>
2024-04-12 11:11:13 +0900

    base frame

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                VStack {
+                    Text("00:00")
+                        .font(.system(size: 20, weight: .bold))
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "play.fill")
+                    }
+                }
+            }
         }
+        .frame(width: 100, height: 100)
         .padding()
     }
 }
