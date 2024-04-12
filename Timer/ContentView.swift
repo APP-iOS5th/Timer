6c925be3b3c64f473e069d18c244628dfe156cdc
YACHAE <104hsh76@gmail.com>
2024-04-12 14:15:43 +0900

    ver.1

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -27,16 +27,22 @@ struct AlwaysOnTopView: NSViewRepresentable {
     }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
 class SoundManager {
     static let instance = SoundManager()
     var player: AVAudioPlayer?
     
     func playSound() {
-        guard let url = Bundle.main.url(forResource: "Beep", withExtension: "mov") else { return }
+        guard let url = Bundle.main.url(forResource: "beep", withExtension: "mp3") else { return }
         
         do {
             player = try AVAudioPlayer(contentsOf: url)
+            player?.numberOfLoops = -1 // 무한반복
             player?.play()
         } catch let error {
             print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
@@ -48,73 +54,96 @@ class SoundManager {
 struct ContentView: View {
     @State private var isRunning = false
     @State private var timeRemaining = 0
+    @State private var isRed = false // 빨간불
+    @State private var sliderValue: Double = 0 // 슬라이더
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
-                
-                Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
-                    .stroke(Color.blue, lineWidth: 10)
-                    .rotationEffect(.degrees(-90))
-                
-                VStack {
-                    Button {
-                        switch timeRemaining {
-                        case 0..<180:
-                            timeRemaining = 180
-                        case 180..<300:
-                            timeRemaining = 300
-                        case 300..<420:
-                            timeRemaining = 420
-                        case 300..<600:
-                            timeRemaining = 600
-                        case 600..<900:
-                            timeRemaining = 900
-                        case 900..<1200:
-                            timeRemaining = 1200
-                        case 1200..<1500:
-                            timeRemaining = 1500
-                        case 1500..<1800:
-                            timeRemaining = 1800
-                        default:
-                            timeRemaining = 0
-                        }
-
-                    } label: {
+        
+        ZStack {
+            Color.red
+                .opacity(isRed ? 0.5 : 0)
+                .animation(.easeInOut(duration: 0.5), value: isRed)
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    Circle()
+                        .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                    
+                    Circle()
+                        .trim(from: 0, to: CGFloat(timeRemaining) / (30 * 60))
+                        .stroke(Color.blue, lineWidth: 10)
+                        .rotationEffect(.degrees(-90))
+                    
+                    VStack {
                         Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
                             .font(.system(size: 20, weight: .bold))
+                        
+                        Button {
+                            isRunning.toggle()
+                            if isRunning && timeRemaining <= 10 {
+                                isRed = true // 재생을 누르고, 10초 이하면 빨간색 활성화
+                                SoundManager.instance.playSound()
+                            } else {
+                                isRed = false // 그 외의 경우 빨간색 비활성화
+                                SoundManager.instance.player?.stop()
+                            }
+                        } label: {
+                            Image(systemName: isRunning ? "pause" : "play.fill")
+                        }
                     }
-                    .buttonStyle(PlainButtonStyle())
-                    Button {
-                        isRunning.toggle()
-                    } label: {
-                        Image(systemName: isRunning ? "pause" : "play.fill")
-                    }
+                    
                 }
+                .frame(width: 150, height: 150)
+                .padding()
+                Slider(value: $sliderValue, in: 0...900, step: 1)
+                    .padding()
+                    .frame(width: 300)
+                    .onChange(of: sliderValue) { [sliderValue] in
+                        timeRemaining = Int(sliderValue)
+                    }
+                    .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
+                    .onReceive(timer) { _ in
+                        if isRunning && timeRemaining > 0 {
+                            timeRemaining -= 1
+                            sliderValue = Double(timeRemaining)
+                            
+                            if timeRemaining <= 10 { // 타이머가 10초 이하일때만 AlwaysOnTop
+                                isRed.toggle()
+                                NSApplication.shared.windows.first?.level = .floating
+                                if isRed {
+                                    SoundManager.instance.playSound()
+                                } else {
+                                    SoundManager.instance.player?.stop()
+                                }
+                                
+                            } else {
+                                NSApplication.shared.windows.first?.level = .normal
+                                SoundManager.instance.player?.stop()
+                            }
+                        } else if isRunning {
+                            isRunning = false
+                            isRed = false
+                            NSApplication.shared.windows.first?.level = .normal
+                            SoundManager.instance.player?.stop()
+                        }
+                    }
             }
-                
         }
-        .frame(width: 100, height: 100)
-        .padding()
-        .background(AlwaysOnTopView(window: NSApplication.shared.windows.first!, isAlwaysOnTop: true))
-        .onReceive(timer) { _ in
-            if isRunning && timeRemaining > 0 {
-                timeRemaining -= 1
-                if timeRemaining <= 10 {
-                    NSSound.beep()
-                }
-            } else if isRunning {
-                isRunning = false
+        .onAppear {
+            // 창의 최소 크기와 최대 크기를 설정합니다.
+            // 예를 들어, 가로 길이를 400pt로 고정하고자 한다면:
+            if let window = NSApplication.shared.windows.first {
+                window.styleMask.insert(.resizable)
+                window.minSize = CGSize(width: 200, height: window.minSize.height)
+                window.maxSize = CGSize(width: 200, height: window.maxSize.height)
             }
         }
     }
 }
-
 #Preview {
     ContentView()
 }
+
