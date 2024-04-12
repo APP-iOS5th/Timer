54af5ff007b6d5b54d742ee242936fd71416bde0
uunwon <chea2089@naver.com>
2024-04-12 11:02:12 +0900

    Initial Commit

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by uunwon on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
