5a7c17e851be89d2aa26fd473a284bc4e3df0274
jinwoong <baseball0020@gmail.com>
2024-04-12 23:10:24 +0900

    feat: add `TimerSelectView`

--- Timer/View/TimeSelectView.swift
+++ Timer/View/TimeSelectView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TimeSelectView: View {
+    @State private var minute = 0
+    @State private var second = 0
+    
+    @Environment(\.dismiss) var dismiss
+    
+    private let viewModel: TimerListViewModel
+    
+    init(viewModel: TimerListViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(spacing: 0) {
+                    NumericTextField(number: $minute)
+                    Text("minutes")
+                }
+                VStack(spacing: 0) {
+                    NumericTextField(number: $second)
+                    Text("seconds")
+                }
+            }
+            Button {
+                viewModel.append(with: TimerInfo(time: Double(minute * 60 + second)))
+                dismiss()
+            } label: {
+                Text("Add")
+            }
+            .buttonStyle(AddButtonStyle())
+        }
+        .frame(width: 300, height: 200)
     }
 }
 
 #Preview {
-    TimeSelectView()
+    TimeSelectView(
+        viewModel: TimerListViewModel()
+    )
 }
