7827b1a57871cc8d69cef84e64ab79ed6e17286e
Shiene Park <logintolookupsome@gmail.com>
2024-04-12 16:05:59 +0900

    base frame

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -10,8 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            ZStack {
+                Circle()
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                VStack {
+                    Text("00:00")
+                        .font(.system(size: 20, weight: .bold))
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "play.fill")
+                    }
+                }
+                
+            }
         }
+        .frame(width: 100, height: 100)
+        .padding()
         
     }
 }
