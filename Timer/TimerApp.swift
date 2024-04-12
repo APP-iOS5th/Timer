adac02b0ef4e48af77b2d78c164b5e23b7569ece
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-12 10:03:05 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 황민경 on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
