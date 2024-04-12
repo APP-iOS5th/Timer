85412cb1345d963eb495afb5df19a70450e94fac
jinwoong <baseball0020@gmail.com>
2024-04-12 16:47:25 +0900

    feat: add `TimerListView`

--- Timer/View/TimerListView.swift
+++ Timer/View/TimerListView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct TimerListView: View {
+    @State private var timerInfo: [TimerInfo] = [
+        .init(time: 30),
+        .init(time: 180),
+        .init(time: 10),
+        .init(time: 50),
+        .init(time: 120),
+    ]
+    
+    private var columns: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                Text("Timer List")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                LazyVGrid(columns: columns) {
+                    ForEach(timerInfo) { timer in
+                        NavigationLink {
+                            TimerView(timer)
+                        } label: {
+                            TimerCell(timer.time)
+                        }
+
+                    }
+                }
+            }
+            .padding(.top)
+        }
+        .frame(width: 300, height: 300)
+        .buttonStyle(.plain)
+        .background(Color.black)
     }
 }
 
