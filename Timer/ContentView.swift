9c65fa678b6eef4e7cc5047757c1cbba1079cc57
s2ee <danwater86@gmail.com>
2024-04-13 21:00:58 +0900

    Initial Commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by Mac on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Mac on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
